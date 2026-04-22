85ecea25faa9c1e65671bd111487aa04e9ac96fa
Rishav Choudhary <rishavchoudhary@Rishavs-MacBook-Air.local>
2026-04-22 23:43:39 +0530

    Added: the render deployed url

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -2,8 +2,8 @@ import { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
-export const API_URL = "http://localhost:8000";
-// export const API_URL = "https://interview-prep-ai-yrvv.onrender.com";
+// export const API_URL = "http://localhost:8000";
+export const API_URL = "https://rish-interprep-backend.onrender.com"
 
 
 export const AuthProvider = ({ children }) => {
